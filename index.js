996c1c24d9f49a27eebb67091e677c10127b20bb
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-02-28 11:06:04 +0530

    cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 let users = [
@@ -18,7 +20,6 @@ let users = [
 ];
 
 let Authusers = [];
-
 
 //signup
 
